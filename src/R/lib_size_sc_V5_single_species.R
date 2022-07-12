70e5dd453a4d9920706902cb43a58ca01deafa93
Neva Durand <nchernia@gmail.com>
2022-07-12 10:27:22 -0400

    - Adding vertical line at elbow to libSizes plot

--- src/R/lib_size_sc_V5_single_species.R
+++ src/R/lib_size_sc_V5_single_species.R
@@ -2,6 +2,28 @@
 
 args <- commandArgs()
 # print(args)
+elbow_finder <- function(x_values, y_values) {
+  # Max values to create line
+  max_x_x <- max(x_values)
+  max_x_y <- y_values[which.max(x_values)]
+  max_y_y <- max(y_values)
+  max_y_x <- x_values[which.max(y_values)]
+  max_df <- data.frame(x = c(max_y_x, max_x_x), y = c(max_y_y, max_x_y))
+  # Creating straight line between the max values
+  fit <- lm(max_df$y ~ max_df$x)
+  # Distance from point to line
+  distances <- numeric(length(x_values))
+  
+  for(i in 1:length(x_values)) {
+    distances[i] <- abs(coef(fit)[2]*x_values[i] - y_values[i] + coef(fit)[1]) / sqrt(coef(fit)[2]^2 + 1^2)
+  }
+  
+  # Max distance point
+  x_max_dist <- x_values[which.max(distances)]
+  y_max_dist <- y_values[which.max(distances)]
+  
+  return(c(x_max_dist, y_max_dist))
+}
 
 unfilt <- args[6]
 filt <- args[7]
@@ -62,7 +84,7 @@ alllibeize <- sum(Df[ ,ncol+2])
 duprate <- mean(Df[Df[, ncol+2] > 500,ncol+1])
 allreads <- sum(Df[ ,ncol])
 dupreads <- sum(Df[ ,ncol]) - sum(Df[ ,ncol-1])
-out = data.frame(LIBRARY="Unkown",
+out = data.frame(LIBRARY="Unknown",
          UNPAIRED_READS_EXAMINED=0,
          READ_PAIRS_EXAMINED=allreads ,
          SECONDARY_OR_SUPPLEMENTARY_RDS=0,
@@ -84,7 +106,13 @@ print("Plot Libsize, Species mixing etc...")
 Size2 <-Df[, ncol+2];  Size2 <- Size2[Size2 >0]; # plot(Size)
 file6 <- paste(basename(filt), 'libSize.pdf', sep=".")
 pdf(file6)
-plot(log10(sort(Size2, decreasing = T)), xlab="Barcode rank", ylab = "log10 (lib size)", main = "Estimated Lib Sizes per Cell", col="darkblue", pch=16)
+par(mfrow = c(2,1))
+Size2 <- sort(Size2, decreasing = T)
+elbow <- elbow_finder(1:length(Size2), log10(Size2))
+plot(log10(Size2), xlab=paste0("Barcode rank (", length(Size2)-elbow[1], " low quality cells)"), ylab = "log10 (lib size)", main = "Estimated Lib Sizes per Cell", col="darkblue", pch=16)
+abline(v = elbow[1])
+axis(1, at=elbow[1], labels=elbow[1])
+plot(log10(Size2[1:elbow[1]]), xlab="Barcode rank", ylab = "log10 (lib size)", main = "Estimated Lib Sizes per high quality cell", col="darkblue", pch=16)
 garbage <- dev.off()
 
 file12 <- paste(basename(filt), 'FilvsUnfil.pdf', sep=".")
