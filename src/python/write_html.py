55af459316d6c23ce0c918bbbc8b8cd0a99f1fc8
Neva Durand <nchernia@gmail.com>
2022-07-18 16:10:39 -0400

    newer python

--- src/python/write_html.py
+++ src/python/write_html.py
@@ -7,8 +7,8 @@ Write output HTML file from list of images and text
 import argparse
 import base64
 
-def main(image_files, text):
-    html = '<html><body><p>' + text + '</p>'
+def main(image_files, log_files):
+    html = '<br>'
 
     image_list = image_files.split(",")
     for image in image_list:
@@ -16,8 +16,20 @@ def main(image_files, text):
         data_base64 = base64.b64encode(data)  # encode to base64 (bytes)
         data_base64 = data_base64.decode()    # convert bytes to string
         html = html + '<img src="data:image/jpeg;base64,' + data_base64 + '"><br>' # embed in html
-    html = html + '</body></html>'
-    open('output.html', 'w').write(html)
+    f = open('output.html', 'a+')
+    f.write(html)
+
+    f.write('<pre>')
+    log_list = log_files.split(",")
+    for log in log_list:
+        f.write('<h3>')
+        f.write(log)
+        f.write('</h3>')
+        f.write('<pre>')
+	f1 = open(log, 'r')
+        f.write(f1.read())
+        f.write('</pre>)
+    f.write('</body></html>')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -26,8 +38,8 @@ if __name__ == '__main__':
     group = parser.add_argument_group()
     group.add_argument('image_files', 
                        help='comma separated list of image files to paste in HTML file')
-    group.add_argument('text',
-                       help='text to write at beginning of HTML file')
+    group.add_argument('log_files',
+                       help='comma separated list of text log files to append to end of HTML file')
     args = parser.parse_args()
-    main(args.image_files, args.text)
+    main(args.image_files, args.log_files)
 
