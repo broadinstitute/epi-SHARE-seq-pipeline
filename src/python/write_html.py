73f769ade8c1934273e98290723043f794cdd7a7
Neva Durand <nchernia@gmail.com>
2022-07-20 10:22:29 -0400

    - changing logic to take list of files

--- src/python/write_html.py
+++ src/python/write_html.py
@@ -7,12 +7,11 @@ Write output HTML file from list of images and text
 import argparse
 import base64
 
-def main(image_files, log_files):
+def main(image_file_list, log_file_list):
+    image_list = open(image_file_list, 'r')  
     html = '<br>'
-
-    image_list = image_files.split(",")
-    for image in image_list:
-        data = open(image, 'rb').read() # read bytes from file
+    for image in image_list.readlines():
+        data = open(image.strip(), 'rb').read() # read bytes from file
         data_base64 = base64.b64encode(data)  # encode to base64 (bytes)
         data_base64 = data_base64.decode()    # convert bytes to string
         html = html + '<img src="data:image/jpeg;base64,' + data_base64 + '"><br>' # embed in html
@@ -20,13 +19,13 @@ def main(image_files, log_files):
     f.write(html)
 
     f.write('<pre>')
-    log_list = log_files.split(",")
-    for log in log_list:
+    log_list = open(log_file_list, 'r')
+    for log in log_list.readlines():
         f.write('<h3>')
         f.write(log)
         f.write('</h3>')
         f.write('<pre>')
-	f1 = open(log, 'r')
+        f1 = open(log.strip(), 'r')
         f.write(f1.read())
         f.write('</pre>')
     f.write('</body></html>')
@@ -36,10 +35,10 @@ if __name__ == '__main__':
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=__doc__.split('\n\n\n')[0])
     group = parser.add_argument_group()
-    group.add_argument('image_files', 
-                       help='comma separated list of image files to paste in HTML file')
-    group.add_argument('log_files',
-                       help='comma separated list of text log files to append to end of HTML file')
+    group.add_argument('image_file_list', 
+                       help='file containing list of image files to paste in HTML file')
+    group.add_argument('log_file_list',
+                       help='file containing list of text log files to append to end of HTML file')
     args = parser.parse_args()
-    main(args.image_files, args.log_files)
+    main(args.image_file_list, args.log_file_list)
 
