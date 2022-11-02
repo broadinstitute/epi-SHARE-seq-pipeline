c1cdc086ccdc87bf8049fe62f454d79522b00691
mei-knudson <mknudson@broadinstitute.org>
2022-11-02 15:43:42 -0400

    - Removing capitalized variables; adding skip_header argument to get_split_lines; parameterizing UMI cutoff of RNA metrics file; changing single letter variable names

--- src/python/joint_cell_plotting.py
+++ src/python/joint_cell_plotting.py
@@ -17,16 +17,19 @@ def parse_arguments():
     parser.add_argument("pkr", help="PKR name")
     parser.add_argument("rna_metrics_file", help="Filename for RNA metrics tsv matrix")
     parser.add_argument("atac_metrics_file", help="Filename for ATAC metrics tsv file")
-    parser.add_argument("min_UMIs", type=int, help="Cutoff for minimum number of UMIs")
+    parser.add_argument("umi_metrics_cutoff", type=int, help="Cutoff for minimum number of UMIs to be applied to RNA barcode metrics file")
+    parser.add_argument("min_umis", type=int, help="Cutoff for minimum number of UMIs")
     parser.add_argument("min_genes", type=int, help="Cutoff for minimum number of genes")
-    parser.add_argument("min_TSS", type=int, help="Cutoff for minimum TSS score")
+    parser.add_argument("min_tss", type=int, help="Cutoff for minimum TSS score")
     parser.add_argument("min_frags", type=int, help="Cutoff for minimum number of ATAC fragments")
     parser.add_argument("barcode_metadata_file", help="Filename for barcode metadata csv file")
     
     return parser.parse_args()
 
-def get_split_lines(file_name, delimiter):
+def get_split_lines(file_name, delimiter, skip_header):
     with open(file_name, "r") as f:
+        if skip_header:
+            next(f)
         for line in f:
             yield line.rstrip().split(sep=delimiter)
 
@@ -37,45 +40,45 @@ def merge_dicts(dict_1, dict_2):
     
     return(merged)
 
-def get_metrics(rna_metrics_file, atac_metrics_file):
+def get_metrics(rna_metrics_file, atac_metrics_file, umi_metrics_cutoff):
     """Read files and aggregate metrics into Pandas dataframe"""
-    rna_metrics_contents = get_split_lines(rna_metrics_file, delimiter="\t")
-    UMIs = []
+    rna_metrics_contents = get_split_lines(rna_metrics_file, delimiter="\t", skip_header=True)
+    umis = []
     genes = []
     rna_barcodes = []
-    next(rna_metrics_contents) # skip header
+    # RNA metrics file contains all UMI values, while ATAC metrics file only uses frags ≥ 10;
+    # impose UMI cutoff (default=10) to keep plot consistent
     for line in rna_metrics_contents:
-        if int(line[1]) >= 10: # impose cutoff of 10 UMIs; RNA metrics file contains all values
-            UMIs.append(int(line[1]))
+        if int(line[1]) >= umi_metrics_cutoff: 
+            umis.append(int(line[1]))
             genes.append(int(line[2]))
             rna_barcodes.append(line[5])
-    rna_metrics = dict(zip(rna_barcodes, zip(UMIs, genes)))
+    rna_metrics = dict(zip(rna_barcodes, zip(umis, genes)))
     
-    atac_metrics_contents = get_split_lines(atac_metrics_file, delimiter="\t")
-    TSS = []
+    atac_metrics_contents = get_split_lines(atac_metrics_file, delimiter="\t", skip_header=True)
+    tss = []
     frags = []
     atac_barcodes = []
-    next(atac_metrics_contents) # skip header 
     for line in atac_metrics_contents: 
-        TSS.append(float(line[1]))
+        tss.append(float(line[1]))
         frags.append(int(line[10]))
         atac_barcodes.append(line[15].split("#")[1]) # get barcode string after library name
-    atac_metrics = dict(zip(atac_barcodes, zip(TSS, frags)))
+    atac_metrics = dict(zip(atac_barcodes, zip(tss, frags)))
     
     # merge metrics by barcodes
     metrics = merge_dicts(rna_metrics, atac_metrics)
-    df = pd.DataFrame.from_dict(metrics, orient="index", columns=["UMIs","genes","TSS","frags"])
+    df = pd.DataFrame.from_dict(metrics, orient="index", columns=["umis","genes","tss","frags"])
     
     return(df)
 
-def qc_cells(df, min_UMIs, min_genes, min_TSS, min_frags):
-    u = df["UMIs"] >= min_UMIs
-    g = df["genes"] >= min_genes
-    t = df["TSS"] >= min_TSS
-    f = df["frags"] >= min_frags
+def qc_cells(df, min_umis, min_genes, min_tss, min_frags):
+    pass_umis = df["umis"] >= min_umis
+    pass_genes = df["genes"] >= min_genes
+    pass_tss = df["tss"] >= min_tss
+    pass_frags = df["frags"] >= min_frags
     
     # add df column with QC outcome
-    qc_conditions  = [(u & g & t & f), (u & g), (t & f)]
+    qc_conditions  = [(pass_umis & pass_genes & pass_tss & pass_frags), (pass_umis & pass_genes), (pass_tss & pass_frags)]
     qc_choices = ["both", "RNA only", "ATAC only"]
     df["QC"] = np.select(qc_conditions, qc_choices, default="neither")
     
@@ -93,16 +96,16 @@ def round_to_power_10(x):
 def label_func(breaks):
     return [int(x) for x in breaks]
     
-def plot_cells(df, pkr, min_UMIs, min_genes, min_TSS, min_frags):
+def plot_cells(df, pkr, min_umis, min_genes, min_tss, min_frags):
     # get max x and y coords to set plot limits
     max_x = max(df["frags"])
-    max_y = max(df["UMIs"])
+    max_y = max(df["umis"])
     xy_lim = round_to_power_10(max(max_x, max_y))
     
-    plot = (ggplot(df, aes("frags", "UMIs", color="QC_count"))
+    plot = (ggplot(df, aes("frags", "umis", color="QC_count"))
              + geom_point(size=0.5)
              + labs(title = f"Joint Cell Calling ({pkr})",
-                    caption = f"ATAC cutoffs: TSS ≥ {min_TSS}, frags ≥ {min_frags}. RNA cutoffs: UMIs ≥ {min_UMIs}, genes ≥ {min_genes}",
+                    caption = f"ATAC cutoffs: TSS ≥ {min_tss}, frags ≥ {min_frags}. RNA cutoffs: UMIs ≥ {min_umis}, genes ≥ {min_genes}",
                     x = "ATAC Unique Fragments per Barcode",
                     y = "RNA UMIs per Barcode",
                     color = "QC")
@@ -132,23 +135,24 @@ def main():
     pkr = getattr(args, "pkr")
     rna_metrics_file = getattr(args, "rna_metrics_file")
     atac_metrics_file = getattr(args, "atac_metrics_file")
-    min_UMIs = getattr(args, "min_UMIs")
+    umi_metrics_cutoff = getattr(args, "umi_metrics_cutoff")
+    min_umis = getattr(args, "min_umis")
     min_genes = getattr(args, "min_genes")
-    min_TSS = getattr(args, "min_TSS")
+    min_tss = getattr(args, "min_tss")
     min_frags = getattr(args, "min_frags")
     barcode_metadata_file = getattr(args, "barcode_metadata_file")
     
     # read rna and atac files, get cell metrics
     logging.info("Getting metrics\n")
-    metrics_df = get_metrics(rna_metrics_file, atac_metrics_file)
+    metrics_df = get_metrics(rna_metrics_file, atac_metrics_file, umi_metrics_cutoff)
     
     # QC cells based on inputted cutoffs
     logging.info("QCing cells\n")
-    metrics_df = qc_cells(metrics_df, min_UMIs, min_genes, min_TSS, min_frags)
+    metrics_df = qc_cells(metrics_df, min_umis, min_genes, min_tss, min_frags)
     
     # generate plot
     logging.info("Generating joint cell calling plot\n")
-    plot_cells(metrics_df, pkr, min_UMIs, min_genes, min_TSS, min_frags)
+    plot_cells(metrics_df, pkr, min_umis, min_genes, min_tss, min_frags)
     
     # save dataframe
     logging.info("Saving dataframe as csv\n")
