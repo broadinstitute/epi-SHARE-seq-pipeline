8724359df9912044549a62cb74383f1cde8bc69b
mei-knudson <mknudson@broadinstitute.org>
2024-03-25 16:28:50 -0400

    - Removing shift

--- src/python/utils.py
+++ src/python/utils.py
@@ -5,37 +5,23 @@ Utility functions
 """
 
 
-def check_putative_barcode(barcode_str, quality_str, barcode_exact_dict, barcode_mismatch_dict):
+def check_putative_barcode(barcode_str, barcode_exact_dict, barcode_mismatch_dict):
     """
-    Procedure: check exact match of barcode, then 1 mismatch, then 1bp left/right
-    shift
+    Check exact match of barcode, then 1 mismatch. Return corrected
+    barcode and boolean indicating whether the correction was an exact match.
+    If no match was found, return None for the corrected barcode.
     """
-    correction_type = None
-    corrected = barcode_exact_dict.get(barcode_str[1:9])  # check exact location first
-    quality = quality_str[1:9]
+    corrected = barcode_exact_dict.get(barcode_str)  # check exact location first
+
     if corrected:
-        correction_type = "E"
-    else:
-        corrected = barcode_mismatch_dict.get(barcode_str[1:9])  # check mismatch
-        if corrected:
-            correction_type = "M"
-        else:
-            corrected = barcode_exact_dict.get(barcode_str[:8])  # check 1bp shift left
-            quality = quality_str[:8]
-            if corrected:
-                correction_type = "L"
-            else:  # check 1bp shift right; round 3 is shorter so add "N" for those
-                if len(barcode_str) < 10:
-                    corrected = barcode_mismatch_dict.get(barcode_str[2:]+"N")
-                    quality = quality_str[2:]+"F"
-                    if corrected:
-                        correction_type = "R"
-                else:
-                    corrected = barcode_exact_dict.get(barcode_str[2:])
-                    quality = quality_str[2:]
-                    if corrected:
-                        correction_type = "R"
-    return corrected, quality, correction_type
+        return corrected, True
+    
+    corrected = barcode_mismatch_dict.get(barcode_str)  # check mismatch
+
+    if corrected:
+        return corrected, False
+    
+    return None, False
 
 def create_barcode_dicts(barcode_list):
     """
@@ -46,7 +32,7 @@ def create_barcode_dicts(barcode_list):
     for barcode in barcode_list:
         barcode_exact_dict[barcode] = barcode  # exact match
         for i, base in enumerate(barcode):
-            for x in 'ACGTN':
+            for x in 'ACGT':
                 if base != x:
                     # add mismatch possibilities at pos i
                     mismatch = barcode[:i] + x + barcode[i + 1:]
